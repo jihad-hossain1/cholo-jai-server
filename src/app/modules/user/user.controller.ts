74f91d49f7821fd77fe9c629806948128af2812a
jihad-hossain1 <jihadkhan934@gmail.com>
2024-05-06 22:05:10 +0600

    user validation added

--- src/app/modules/user/user.controller.ts
+++ src/app/modules/user/user.controller.ts
@@ -1,86 +1,91 @@
-import { Request, Response } from 'express';
+
+import { Request, Response } from "express";
 import { PrismaClient } from "@prisma/client";
+import { validateDateOfBirth, validateField, validateMobile,validateGender,validateOccupation,validateUserType } from "../../../helpers/inputFieldValidation";
+import bcrypt from 'bcrypt';
 
 const prisma = new PrismaClient();
 
-
-const createUser = async(req: Request, res: Response): Promise<Response> => {
-  const reqFromBody = req.body
-  const { fullName, mobile, email, password, dateOfBirth, gender, age, role, occupation, address } = reqFromBody;
+const createUser = async (req: Request, res: Response)=> {
+  const reqFromBody = req.body;
   
-  // if (!fullName || fullName === undefined || fullName === null || fullName === '') {
-  //   return res.status(400).json({ message: 'fullName is required' });
-  // } else if (!mobile || mobile === undefined || mobile === null || mobile === '') {
-  //   return res.status(400).json({ message: 'mobile is required' });
-  // } else if (!email || email === undefined || email === null || email === '') {
-  //   return res.status(400).json({ message: 'email is required' });
-  // } else if (!password || password === undefined || password === null || password === '') {
-  //   return res.status(400).json({ message: 'password is required' });
-  // } else if (!dateOfBirth || dateOfBirth === undefined || dateOfBirth === null || dateOfBirth === '') {
-  //   return res.status(400).json({ message: 'dateOfBirth is required' });
-  // } else if (!gender || gender === undefined || gender === null || gender === '') {
-  //   return res.status(400).json({ message: 'gender is required' });
-  // } else if (!age || age === undefined || age === null || age === '') {
-  //   return res.status(400).json({ message: 'age is required' });
-  // } else if (!role || role === undefined || role === null || role === '') {
-  //   return res.status(400).json({ message: 'Type is required' });
-  // } else if (!occupation || occupation === undefined || occupation === null || occupation === '') {
-  //   return res.status(400).json({ message: 'occupation is required' });
-  // } else if (!address || address === undefined || address === null || address === '') {
-  //   return res.status(400).json({ message: 'address is required' });
-  // } 
-
-
+  const {
+    fullName,
+    mobile,
+    email,
+    password,
+    dateOfBirth,
+    gender,
+    age,
+    role,
+    occupation,
+    address,
+  } = reqFromBody;
 
   try {
+  
+  validateField(fullName, "Full Name", 1, 50);
+  validateField(email, "Email", 1, 50);
+  validateField(password, "Password", 6, 30);
+  validateOccupation(occupation);
+  validateUserType(role);
+  validateField(address, "Address", 1, 250);
+  validateDateOfBirth(dateOfBirth);
+  validateMobile(mobile);
+  validateGender(gender);
 
-    const userExist = await prisma.user.findFirst({
+  const userEmailExist = await prisma.user.findFirst({
       where: {
-        email: email
-      }
-    })
-
-    if (userExist) {
-      return res.status(400).json({ message: 'User already exist' });
-    }
-
-    const mobileExist = await prisma.user.findFirst({
-      where: {
-        mobile: mobile
-      }
-    })
-
-    if (mobileExist) {
-      return res.status(400).json({ message: 'Mobile number already exist' });
-    }
-
+        email: email,
+      },
+  });
     
-    const trimEmail = await email.split('@')[0];
+    if (userEmailExist) {
+      return res.status(400).json({ message: "Email already exist" });
+    }
+
+    const userMobileExist = await prisma.user.findFirst({
+        where: {
+            mobile: mobile
+        }
+    })
+
+    if (userMobileExist) {
+        return res.status(400).json({ message: "Mobile already exist" });
+    }
+
+    const trimEmail = await email.split("@")[0];
+
+    const generatePassword = await bcrypt.hash(password, 10);
+    console.log(generatePassword);
 
     const newUser = await prisma.user.create({
-      data: 
-      {
+      data: {
         fullName: fullName,
         mobile: mobile,
         email: email,
-        password: password,
+        password: generatePassword,
         dateOfBirth: dateOfBirth,
         gender: gender,
         age: age,
         occupation: occupation,
         role: role,
         userName: trimEmail,
-        address: address
-      }
-     })
-    console.log(newUser)
+        address: address,
+      },
+    });
 
     return res.status(201).json({ result: newUser });
+
   } catch (error) {
-   return res.status(400).json({ message: error }); 
+    console.log(error);
+  if (error instanceof Error) {
+    return res.status(400).json({ message: error.message });
   }
 }
 
+
+};
 
 export const UserController = {
   createUser,
