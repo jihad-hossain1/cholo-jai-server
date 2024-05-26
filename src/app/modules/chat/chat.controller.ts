79ca05e7572620f12268968ee980382bf76fd786
jihad-hossain1 <jihadkhan934@gmail.com>
2024-05-27 00:41:01 +0600

    asd

--- src/app/modules/chat/chat.controller.ts
+++ src/app/modules/chat/chat.controller.ts
@@ -2,6 +2,8 @@
 // import { LocationService } from "./location.service";
 // import httpStatus from "http-status";
 
+import { Request, Response } from "express";
+
 // const createChat = async (req: Request, res: Response) => {
 //   try {
 //     const data = await req.body;
@@ -21,3 +23,63 @@
 //     });
 //   }
 // };
+const { PrismaClient } = require("@prisma/client");
+
+const prisma = new PrismaClient();
+
+const createChat = async (req: Request, res: Response) => {
+  const { senderUserId, receiverUserId } = req.body;
+
+  if (!senderUserId) {
+    console.log("senderUserId param not sent with request");
+    return res.sendStatus(400);
+  }
+
+  const chat = await prisma.chat.findFirst({
+    where: {
+      isGroupChat: false,
+      AND: [
+        { users: { some: { id: receiverUserId } } },
+        { users: { some: { id: senderUserId } } },
+      ],
+    },
+    include: {
+      users: {
+        select: { id: true, name: true, pic: true, email: true },
+      },
+      latestMessage: {
+        include: {
+          sender: {
+            select: { name: true, pic: true, email: true },
+          },
+        },
+      },
+    },
+  });
+
+  if (chat) {
+    res.send(chat);
+  } else {
+    const chatData = {
+      chatName: "sender",
+      isGroupChat: false,
+      users: {
+        connect: [{ id: receiverUserId }, { id: senderUserId }],
+      },
+    };
+
+    try {
+      const createdChat = await prisma.chat.create({
+        data: chatData,
+        include: {
+          users: true,
+        },
+      });
+
+      res.status(200).send(createdChat);
+    } catch (error: any) {
+      res.status(400);
+      throw new Error(error.message);
+    }
+  }
+};
